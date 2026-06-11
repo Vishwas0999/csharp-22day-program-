93f2733534e4b9187785086f857d765ccc13bca8
Vishwas0999 <vv0714153@gmail.com>
2026-06-11 04:04:07 -0700

    Commits

---
+++ Day07/Labs/RegistrationFoundation/RegistrationFoundation/wwwroot/bed-dashboard.js
@@ -0,0 +1,82 @@
+﻿// ===================================================
+// Hospital Bed Availability Dashboard
+// Demonstrates:
+// - Arrays
+// - Objects
+// - Loops
+// - Conditions
+// - DOM manipulation
+// ===================================================
+
+
+// -----------------------------
+// BED DATA (Mock backend data)
+// -----------------------------
+let beds = [
+    { bedNumber: 1, isOccupied: false },
+    { bedNumber: 2, isOccupied: true },
+    { bedNumber: 3, isOccupied: false },
+    { bedNumber: 4, isOccupied: true },
+    { bedNumber: 5, isOccupied: false },
+    { bedNumber: 6, isOccupied: false },
+    { bedNumber: 7, isOccupied: true },
+    { bedNumber: 8, isOccupied: false },
+    { bedNumber: 9, isOccupied: true },
+    { bedNumber: 10, isOccupied: false },
+    { bedNumber: 11, isOccupied: false },
+    { bedNumber: 12, isOccupied: false }
+];
+
+
+// -----------------------------
+// FUNCTION: Render beds on screen
+// -----------------------------
+let occ = 0;
+
+function renderBeds() {
+
+    let container = document.getElementById("bedContainer");
+
+    // Clear existing beds
+    container.innerHTML = "";
+
+    // Loop through all beds
+    for (let i = 0; i < beds.length; i++) {
+
+        let bed = beds[i];
+
+        // Create a div for each bed
+        let bedDiv = document.createElement("div");
+
+        // Assign common bed class
+        bedDiv.classList.add("bed");
+
+        // Condition to decide color
+        if (bed.isOccupied) {
+            bedDiv.classList.add("occupied");
+            bedDiv.innerText = "Bed " + bed.bedNumber + "\nOccupied";
+        } else {
+            bedDiv.classList.add("available");
+            bedDiv.innerText = "Bed " + bed.bedNumber + "\nAvailable";
+        }
+
+        if (!bed.isOccupied) {
+            occ++;
+            // Click event to toggle bed status
+            bedDiv.onclick = function () {
+                bed.isOccupied = !bed.isOccupied;
+                renderBeds(); // Re-render UI
+            };
+        }
+
+        // Add bed to container
+        container.appendChild(bedDiv);
+    }
+}
+let sum = document.createElement("count");
+bedDiv.classList.add(occ + "/12");
+
+// -----------------------------
+// INITIAL LOAD
+// -----------------------------
+renderBeds();
